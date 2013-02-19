42b32e95d4dedbfd16214404d75d931d36875600
Cedric Winbush <cwvisionnfocus@gmail.com>
2013-02-19 13:04:58 -0500

    why the warning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrialWork::Application.config.secret_token = '2a61c18f4b67d1b6052ddaef8927428843746a75664faac16dbd41c76f9aaf234e8fd9e0b58bf90ebd8067e81a6328a68715ceaa7a8593ab56893e320a6f7a6b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TrialWork::Application.config.session_store :cookie_store, key: '_trial_work_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TrialWork::Application.config.session_store :active_record_store
